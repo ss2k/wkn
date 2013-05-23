9b60ba665ed577221fd6ad1c30f1b0a77b41dbd0
James Lee <j@amesee.com>
2013-05-22 21:33:08 -0400

    Implement edit and update actions for Landing resource

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,6 +1,6 @@
 class LandingsController < ApplicationController
   def create
-    @hotspot = current_user.hotspots.find params[:hotspot_id]
+    @hotspot = find_hotspot
     @landing = @hotspot.landings.build params[:landing]
     if @landing.save
       redirect_to @hotspot
@@ -9,9 +9,31 @@ class LandingsController < ApplicationController
     end
   end
 
+  def edit
+    @hotspot = find_hotspot
+    @landing = @hotspot.landings.find params[:id]
+  end
+
   def new
-    @hotspot = current_user.hotspots.find params[:hotspot_id]
+    @hotspot = find_hotspot
     @landing = @hotspot.landings.new
+  end
+
+  def update
+    @hotspot = find_hotspot
+    @landing = @hotspot.landings.find params[:id]
+    if @landing.update_attributes params[:landing]
+      flash[:notice] = 'The landing page was successfully updated.'
+      redirect_to @hotspot
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def find_hotspot
+    current_user.hotspots.find params[:hotspot_id]
   end
 end
 
--- spec/features/landing_pages_spec.rb
+++ spec/features/landing_pages_spec.rb
@@ -17,12 +17,32 @@ feature 'Landing pages' do
   end
 
   scenario 'Create and submit a new landing page' do
-    click_link 'Create a Landing Page'
-    fill_in 'Title', :with => 'Main Landing Page'
-    fill_in 'Customization', :with => '<p>Hello</p>'
-    click_button 'Create Landing'
+    create_landing_page_with 'Main Landing Page', '<p>Hello</p>'
     expect(page).to have_css '.landing-title', :text => 'Main Landing Page'
     expect(page).to have_css '.landing-markup', :text => 'Hello'
+  end
+
+  scenario 'Edit landing page' do
+    create_landing_page
+    within('.landing') do
+      click_link 'Edit'
+    end
+    fill_in 'Title', :with => 'New Title'
+    fill_in 'Customization', :with => '<p>Abracadabra</p>'
+    click_button 'Update Landing'
+    expect(page).to have_css '.landing-title', :text => 'New Title'
+    expect(page).to have_css '.landing-markup', :text => 'Abracadabra'
+  end
+
+  def create_landing_page
+    create_landing_page_with 'Main Landing Page', '<p>Hello</p>'
+  end
+
+  def create_landing_page_with title, content
+    click_link 'Create a Landing Page'
+    fill_in 'Title', :with => title
+    fill_in 'Customization', :with => content
+    click_button 'Create Landing'
   end
 end
 
